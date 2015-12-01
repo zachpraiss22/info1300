a18129bebe69f6501e92d0765c01be1a4dd35536
karen00zhou <koz2@cornell.edu>
2015-12-01 12:49:01 -0500

    updated contact.php and contact_form.php
    Notifies the user when their email has been sent.

--- php/contact.php
+++ php/contact.php
@@ -1,12 +1,42 @@
 <?php
     //Calls on header.php for <head> tags
     include('header.php');
+    
+    $note= "";
+    if(@$_SESSION['note']){
+	$note = $_SESSION['note'];
+	unset($_SESSION['note']);
+    }
 ?>
+    <script>
+    function verify() {
+	var name = document.getElementById('name');
+	var msg = document.getElementById('msg');
+	var subject = document.getElementById('subject');
+	if ( ! name.value ) {
+	    alert("Please provide your name.");
+	    name.focus();
+	    return false;
+	}
+	if ( ! msg.value ) {
+	    alert("Please provide your message.");
+	    comment.focus();
+	    return false;
+	}
+	if ( ! subject.value ) {
+	    alert("Please provide a subject.");
+	    comment.focus();
+	    return false;
+	}
+	return true;
+    }
+    </script>
 
     <body>
 	<?php
 	//NAVIGATION: calls on navigation.php
 	//Calls on banner.php
+	    $current = "CONTACT";
 	    include('navigation.php');
 	    include('banner.php');
 	?>
@@ -17,9 +47,17 @@
                 <h3>WITH ANY QUESTIONS OR CONCERNS</h3>
                 <br><br>
             </div>
+<<<<<<< Updated upstream
 	    <div id="contact">
 			<p>Feel free to shoot us a message and we will get back to you as soon as possible.</p>
 			<br>
+=======
+	    <div id=contact>
+		<!-- If email has been sent, then $note is echoed-->
+		<div id="status" class="message" <?php if(!@$note){echo 'style="display:none;"';}?>><?php echo @$note;?></div>
+	    
+		<p>Any more questions or concerns? Shoot us an email!</p>
+>>>>>>> Stashed changes
 		<form method="post" onSubmit="return verify()" action='contact_form.php'>
 		    <p class="question">Name:</p>
 		    <input type="text" name="name" id="name"><br>
--- php/contact_form.php
+++ php/contact_form.php
@@ -1,13 +1,25 @@
 <?php
+    //Function to send user back to contact.php with notification
+    function backToContact($note){
+        $_SESSION['note']= $note;
+        echo '<script>location.href="contact.php"</script>';
+    }
     // variables
     $subject = $_POST['subject'] . " from " . $_POST['name'];
     $msg = $_POST['msg'];
-
     // use wordwrap() if lines are longer than 70 characters
     $msg = wordwrap($msg,70);
 
+    
     // send email
     mail("seniorclasscampaign@cornell.edu", $subject ,$msg);
     
+<<<<<<< Updated upstream
     header('Location: contact.php');
 ?>
+=======
+    //Notify user
+    backToTable("Your email has been sent!");
+    
+?>
+>>>>>>> Stashed changes
