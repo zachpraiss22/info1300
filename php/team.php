7d2643cc61e0f383898e3666b4ff0fca7bb5e27f
zachpraiss22 <ztp3@cornell.edu>
2015-12-01 15:25:27 -0500

    Update team.php

--- php/team.php
+++ php/team.php
@@ -12,14 +12,12 @@
 	    include('banner.php');
 	?>
 
-        <div id="container">
+<div id="container">
             <div class="content">
                 <h2>OUR TEAM</h2>
                 <h3>FOR THE CAMPAIGN</h3>
                 <br><br>
-                <p>The Senior Class Campaign (SCC) is an annual fundraising campaign organized by the graduating senior class of Cornell University to celebrate our school spirit, enhance the ties that bind us together as a community, and give up back to the insititution.</p>
-                <br>
-                <p>The Senior Class Campaign (SCC) is an annual fundraising campaign organized by the graduating senior class of Cornell University to celebrate our school spirit, enhance the ties that bind us together as a community, and give up back to the insititution.</p>
+                <p>The Senior Class Campaign (SCC) is organized by a hardworking and dedicated group of seniors who are committed to making sure that the Cornell Class of 2016 has the most successful senior class campaign in the university&#8217;s history.</p>
                 <br><br>
                 <div id="team">
                 <h1>Our E-Board</h1>
@@ -30,7 +28,7 @@
                           <h3>Co-President</h3>
                             <br>
                             <br>
-                            <img src="img/team1.jpg" alt="Garrison Lovely" class="img-circle">
+                            <img src="../img/team1.jpg" alt="Garrison Lovely" class="img-circle">
                         </li>
                         <li>
                           <h2>Natalie Rosseau</h2>
@@ -38,7 +36,7 @@
                           <h3>Co-President</h3>
                             <br>
                             <br>
-                          <img src="img/team2.jpg" alt="Natalie Rosseau" class="img-circle">
+                          <img src="../img/team2.jpg" alt="Natalie Rosseau" class="img-circle">
                         </li>
                         <li>
                           <h2>Justin<br>Watt</h2>
@@ -46,7 +44,7 @@
                           <h3>VP Events</h3>
                             <br>
                             <br>
-                          <img src="img/team3.jpg" alt="Justin Watt" class="img-circle">
+                          <img src="../img/team3.jpg" alt="Justin Watt" class="img-circle">
                         </li>
                         <li>
                           <h2>Catherine McAnney</h2>
@@ -54,7 +52,7 @@
                           <h3>VP Operations</h3>
                             <br>
                             <br>
-                          <img src="img/team4.jpg" alt="Catherine McAnney" class="img-circle">
+                          <img src="../img/team4.jpg" alt="Catherine McAnney" class="img-circle">
                         </li>
                         <li>
                           <h2>Lide<br>Forde</h2>
@@ -62,7 +60,7 @@
                           <h3>VP Outreach</h3>
                             <br>
                             <br>
-                          <img src="img/team5.jpg" alt="Lide Forde" class="img-circle">
+                          <img src="../img/team5.jpg" alt="Lide Forde" class="img-circle">
                         </li>
                         <li>
                           <h2>Carolyn Sussman</h2>
@@ -70,7 +68,7 @@
                           <h3>VP Outreach</h3>
                             <br>
                             <br>
-                          <img src="img/team6.jpg" alt="Garrison Lovely" class="img-circle">
+                          <img src="../img/team6.jpg" alt="Garrison Lovely" class="img-circle">
                         </li>
                         <li>
                           <h2>Aditi Bhowmick</h2>
@@ -78,7 +76,7 @@
                           <h3>VP Marketing</h3>
                             <br>
                             <br>
-                          <img src="img/team7.jpg" alt="Aditi Bhowmick" class="img-circle">
+                          <img src="../img/team7.jpg" alt="Aditi Bhowmick" class="img-circle">
                         </li>
                       </ul>
                  <h1> Our Advisors</h1>
@@ -92,7 +90,7 @@
                             </h3>
                             <br>
                             <br>
-                            <img src="img/advisor1.jpg" alt="Corey Ryan Earle '07" class="img-circle">
+                            <img src="../img/advisor1.jpg" alt="Corey Ryan Earle '07" class="img-circle">
                         </li>
                         <li>
                             <h2>Amanda<br>Massa</h2>
@@ -103,7 +101,7 @@
                             </h3>
                             <br>
                             <br>
-                            <img src="img/advisor2.jpg" alt="Amanda Massa" class="img-circle">
+                            <img src="../img/advisor2.jpg" alt="Amanda Massa" class="img-circle">
                         </li>  
                     </ul>
                 </div><!--team-->
