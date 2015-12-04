80ac663c128f9c71b2be1d9ce49c3f0b4b19aa1a
kmc352 <kmc352@cornell.edu>
2015-12-04 08:39:44 -0500

    fixed error

--- php/button.php
+++ php/button.php
@@ -1,5 +1,5 @@
 <div id="give">
-	<a href="https://www.alumni.cornell.edu/seniorclass/give/" class="button"/>Give</a>
+	<a href="https://www.alumni.cornell.edu/seniorclass/give/" class="button">Give</a>
 </div>
 
 
