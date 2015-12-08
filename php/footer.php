aa10892d38097b0f9d724c42f5084a781a52ebab
zachpraiss22 <ztp3@cornell.edu>
2015-12-07 21:18:49 -0500

    iPad Mobile Friendly

--- php/footer.php
+++ php/footer.php
@@ -56,13 +56,13 @@ footer.php is called on by all the other pages.
                 <a href="http://www.facebook.com/CornellSCC2016/" target="_blank"><img src="img/facebook.png" alt="Facebook logo" class="media"></a>
                 <a href="http://www.instagram.com/cuseniors16/" target="_blank"><img src="img/instagram.png" alt="Instagram logo" class="media"></a>
                 <p class="email"><a class="email" href="mailto:seniorclasscampaign@gcornell.edu.com">seniorclasscampaign@cornell.edu</a></p>
-                <p class="credits">
-                Image Source: <a href="http://www.cornellseniorclasscampaign2016.com" target="blank_">cornellseniorclasscampaign2016.com</a>
-                <br>
-                Gallery Code from: <a href="http://basic-slider.com" target="blank_">basic-slider.com</a>
-                <br>
-                Logos courtesy of Cornell University 
-                </p>
+                    <p class="credits">
+                    Image Source: <a href="http://www.cornellseniorclasscampaign2016.com" target="blank_">cornellseniorclasscampaign2016.com</a>
+                    <br>
+                    Gallery Code from: <a href="http://basic-slider.com" target="blank_">basic-slider.com</a>
+                    <br>
+                    Logos courtesy of Cornell University 
+                    </p>
                 <br>
                 <img src="img/CULogo.png" alt="Cornell logo" id="large_logo">
             </div>
